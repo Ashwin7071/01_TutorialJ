b2a2845b88b547b224875781520e793c03070373
Ashwin Mishra <116173963+Ashwin7071@users.noreply.github.com>
2024-02-07 09:16:10 +0000

    container config

--- 01_Tutorial/test.js
+++ 01_Tutorial/test.js
@@ -1,8 +1,8 @@
 var money=2000;
-var value=money/10;
-function call(money,value)
+function call(money)
 {
     console.log(`I have only ${money}rs to spend`);
+    var value=money/10;
     console.log(`and I can take ${value}`);
 }
 call(2001,2001);
